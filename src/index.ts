6737c8b25353add5ec633373111afddc817acdfb
TeddyGavi <matc.davis@gmail.com>
2023-03-10 21:58:12 -0800

    more set up and basic test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as web3 from "@solana/web3.js";
 import Dotenv from "dotenv";
 Dotenv.config();
+const PROGRAM_ADDRESS = "ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa";
+const PROGRAM_DATA_ADDRESS = "Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod";
 
 // async function main() {
 //   const newKeypair = web3.Keypair.generate();
@@ -22,9 +24,40 @@ function initializeKeypair(): web3.Keypair {
   return keypairFromSecretKey;
 }
 
+async function pingProgram(connection: web3.Connection, payer: web3.Keypair) {
+  const transaction = new web3.Transaction();
+
+  const programId = new web3.PublicKey(PROGRAM_ADDRESS);
+  const programDataPubkey = new web3.PublicKey(PROGRAM_DATA_ADDRESS);
+
+  const instruction = new web3.TransactionInstruction({
+    keys: [
+      {
+        pubkey: programDataPubkey,
+        isSigner: false,
+        isWritable: true,
+      },
+    ],
+    programId,
+  });
+  transaction.add(instruction);
+
+  const signature = await web3.sendAndConfirmTransaction(
+    connection,
+
+    transaction,
+
+    [payer]
+  );
+
+  console.log(signature);
+}
+
 async function main() {
   const payer = initializeKeypair();
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
+  await connection.requestAirdrop(payer.publicKey, web3.LAMPORTS_PER_SOL * 1);
+  await pingProgram(connection, payer);
 }
 
-async function pingProgram(connection: web3.Connection, payer: web3.Keypair) {}
+main();
